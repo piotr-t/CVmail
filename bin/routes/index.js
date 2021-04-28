a2619ef73dec29bd24b0167790c57797cef93bb7
piotr-t <troc.piotr@gmail.com>
2021-04-28 16:18:01 +0200

    ok

---
+++ bin/routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var smtpTransport = require('nodemailer-smtp-transport');
+const nodemailer = require("nodemailer");
+require ('dotenv').config();
+
+
+
+const createTransporter = async () => {
+  //let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  return nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+      user: process.env.EMAIL, 
+      pass: process.env.PASSWORD
+    }
+  }));
+  
+}
+
+let transporter = createTransporter();
+
+router.post('/message', function(req, res, next) {
+    // send mail with defined transport object
+    console.log(req.body ,'body');
+    let info = transporter.then(v=>{
+      v.sendMail({
+        from: `${req.body.emailName}`, // sender address
+        to: process.env.EMAIL, // list of receivers
+        subject: `${req.body.textareaName}?`, // Subject line
+        text: `${req.body.textareaName}?`, // plain text body
+        html: `<b>${req.body.textareaName}</b>`, // html body
+      },(v)=>{/* console.log(v,'v'); */});
+    })
+    res.send({message:'wiadomość została wysłana'});
+});
+
+router.get('/message',(req, res)=>{
+  res.send('https://piotrt.ct8.pl/CV')
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
